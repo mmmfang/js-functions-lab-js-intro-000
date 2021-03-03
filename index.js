fe4d74a461bc34da07896dd4ca5c8377fb4d423c
mmmfang <mae.fang@gmail.com>
2021-03-03 02:45:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy holidays!";
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`;
+  return `Happy holidays, ${name}!`
 }
 
 function happyCustomHolidayTo(holiday, name){
-  return `Happy ${holiday}, to ${name}`!;
+  return `Happy ${holiday}, to ${name}`!
 }
 
 function holidayCountdown(holiday, days){

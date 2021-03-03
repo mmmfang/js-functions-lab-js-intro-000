722613a3f3cae87d329a490416fe3e06c0338526
mmmfang <mae.fang@gmail.com>
2021-03-03 02:45:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 function happyHolidays() {
   return "Happy holidays!";
 }
+
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
+
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, to ${name}`!;
 }
+
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`
 }
